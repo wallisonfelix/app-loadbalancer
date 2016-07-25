b8021b27c0af58fb9bdb9c947bbe0de3f0f00db1
wallison.felix <wallison.felix@gmail.com>
2016-07-25 11:58:50 -0300

    Primeiro commit

---
+++ loadbalancer.js
@@ -0,0 +1,47 @@
+var httpProxy = require('http-proxy');
+var http = require('http');
+var proxy = httpProxy.createProxyServer();
+var fs = require('fs');
+ 
+var IP_DEFAULT = "172.17.0.2";
+
+function readIps(callback) {
+	fs.readFile('../ips.txt', 'utf8', function(err, ips) {	
+		if (err) {
+	       callback(err, null);
+	   	}
+	    console.log(ips); 
+	    var arrayIps = ips.split("\r\n");  
+	    console.log(arrayIps);
+	    callback(null, arrayIps);
+	});
+}
+ 
+function randomUrl(callback) {
+	console.log("ok 2");
+	readIps(function(err, ips) {	
+		if (err) {
+			console.log("ok 3");
+	       callback("http://" + IP_DEFAULT);
+	   	}
+	   	console.log("ok 4");
+	   	callback("http://" + ips[Math.floor(Math.random() * ips.length)]);
+	});
+}
+
+var server = http.createServer(function (req, res) {
+	console.log("ok");
+	randomUrl(function(url) {
+   		proxy.web(req, res, { target: randomUrl() });
+   	});
+});
+
+server.on('upgrade', function (req, socket, head) {
+	console.log("ok");
+	randomUrl(function(url) {			
+	   	proxy.ws(req, socket, head, { target: url });
+	});
+});
+
+server.listen(80);
+console.log("Load Balancer escutando na porta 80!");
