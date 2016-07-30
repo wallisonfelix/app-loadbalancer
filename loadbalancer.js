fdadc15ad46cc33ee66c72b061f1118d0b4d152b
wallison.felix <wallison.felix@gmail.com>
2016-07-30 20:11:18 -0300

    Desfazendo o ajuste anterior para a geração do array de IPs

--- loadbalancer.js
+++ loadbalancer.js
@@ -13,7 +13,7 @@ function readIps(callback) {
 	   	}
 	    var arrayIps = ips.split("\n");
 	    if (arrayIps.indexOf("")) {
-	    	arrayIps = arrayIps.splice(arrayIps.indexOf(""), 1);
+	    	arrayIps.splice(arrayIps.indexOf(""), 1);
 	    }
 	    console.log(arrayIps);
 	    callback(null, arrayIps);
